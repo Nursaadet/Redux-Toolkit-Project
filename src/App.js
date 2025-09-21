7880dc447488618629b341188d704e78b7590fad
Nursaadet <nursaadetozdemir@gmail.com>
2025-09-21 15:37:14 +0300

    feat: wrap App with Redux Provider and import store

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import AppRouter from "./router/AppRouter";
-import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 function App() {
   return (

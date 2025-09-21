5073195817e7b7d56783f70354c3fcfb78f605d8
Nursaadet <nursaadetozdemir@gmail.com>
2025-09-21 17:59:46 +0300

    feat(news): add news slice with async thunk for fetching headlines

--- src/features/newsSlice.jsx
+++ src/features/newsSlice.jsx
@@ -1,14 +1,32 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import axios from "axios"
 
+const initialState = {
+  newsData: [],
+  loading: false,
+  error: false,
+}
 
-const initialState = {};
+export const getNews = createAsyncThunk(
+  "getNews", //? action type
+  async () => {
+    //? api istek fonksiyonu
+    const API_KEY = "8b6cc7d0ec5849dba4b1b5f1b88b034c"
+    const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    const { data } = await axios(url)
+    console.log(data.articles)
+    return data.articles
+  }
+)
 
 const newsSlice = createSlice({
   name: "news",
   initialState,
-  reducers: {},
-});
+  reducers: {
+    
+  },
+})
 
-export const {} = newsSlice.actions;
+export const {} = newsSlice.actions
 
-export default newsSlice.reducer;
+export default newsSlice.reducer
